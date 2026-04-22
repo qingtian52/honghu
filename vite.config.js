52de76abe9d67fe5d0c52c88b229d3d60e6ac784
qingtian52 <942472269@qq.com>
2026-04-22 09:35:06 +0800

    update: logo22

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,12 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-  // 指定项目根目录为 public/，这样 Vite 会去 public/ 里找 index.html
-  root: 'public',
   base: './',
+  server: {
+    port: 5173, // 与你之前的端口保持一致
+    open: true
+  },
   build: {
-    // 打包输出到项目根目录的 dist/ 文件夹
-    outDir: '../dist',
-    // 清空打包目录
-    emptyOutDir: true
+    outDir: 'dist'
   }
-})
+});
